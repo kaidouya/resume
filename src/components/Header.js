9abe9056ef75e1fecf4c821757f31517f18a1e40
pinsen_chao <kaidouya0527@gmail.com>
2020-04-09 00:39:49 +0800

    Modift title

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>品森的個人網站</h1>
         <p>
           A fully responsive site template designed by{' '}
           <a href="https://html5up.net">HTML5 UP</a> and released
